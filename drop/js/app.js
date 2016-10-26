152d5adcac37e6f12a75d769972b54ff5b996c5a
Brian Norris <bnorris@marlinco.com>
2016-10-25 20:53:05 -0500

    Commit of placeholder and file drop

---
+++ drop/js/app.js
@@ -0,0 +1,36 @@
+//$(document).foundation()
+/*
+Dropzone.autoDiscover = false;
+
+var myDropzone = new Dropzone("#dzone", {
+//$("#dropzone").dropzone({
+  //url: "upload.php",
+  maxFilesize: 10,
+  acceptedFiles: "image/png, image/gif, image/jpeg, application/pdf",
+  uploadMultiple: true
+});
+*/
+
+
+Dropzone.options.dzone = {
+  //paramName: "file", // The name that will be used to transfer the file
+  maxFilesize: 15, // MB
+  /*accept: function(file, done) {
+    if (file.name == "justinbieber.jpg") {
+      done("Naha, you don't.");
+    }
+    else { done(); }
+  }*/
+
+  acceptedFiles: "image/png, image/gif, image/jpeg, application/pdf",
+  //uploadMultiple: true,
+  /*init: function() {
+    this.on("successmultiple", function() {
+      console.log("Success");
+    });
+    this.on("error", function () {
+        //alert('Upload Error');
+        alert("Upload Error");
+    });
+  }*/
+}
---
+++ drop/upload.php
@@ -0,0 +1,41 @@
+<?php
+$ds          = DIRECTORY_SEPARATOR;  //1
+
+$storeFolder = 'drops';   //2
+
+
+
+
+//function allowed_file(){
+if (!empty($_FILES)) {
+
+//Add the allowed mime-type files to an 'allowed' array
+ $allowed = array('application/pdf', 'image/gif', 'image/jpeg', 'image/png', 'image/jpg');
+ //$allowed = array('image/jpeg', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation');
+//Check uploaded file type is in the above array (therefore valid)
+    //if(in_array($_FILES['resume']['type'], $allowed) AND in_array($_FILES['reference']['type'], $allowed)){
+    if (in_array($_FILES["file"]["type"], $allowed)) {
+
+   //If filetypes allowed types are found, continue to check filesize:
+
+            if($_FILES["resume"]["size"] < 1000000 AND $_FILES["reference"]["size"] < 1000000 ){
+
+              //if both files are below given size limit, allow upload
+              //Begin filemove here....
+
+                  $tempFile = $_FILES['file']['tmp_name'];          //3
+
+                  $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;  //4
+
+                  $targetFile =  $targetPath. $_FILES['file']['name'];  //5
+
+                  move_uploaded_file($tempFile,$targetFile); //6
+
+            }
+
+    }
+
+
+
+}
+?>
