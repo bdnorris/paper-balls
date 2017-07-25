f1405537effea080f405ef05fce6113f382cd966
Brian Norris <bnorris@marlinco.com>
2017-07-24 19:45:00 -0500

    Binding to range input. New Background.

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
     <!-- <img src="./assets/logo.png"> -->
     <!-- <hello></hello> -->
     <balls :balls="balls"></balls>
-    <button type="button" name="button" v-on:click="addComponent">Add</button>
-    <button type="button" name="button" v-on:click="removeComponent">Remove</button>
+    <!-- <button type="button" name="add" class="dial" v-on:click="addComponent">Add</button>
+    <button type="button" name="remove" class="dial" v-on:click="removeComponent">Remove</button> -->
+    <div class="controls">
+      <input type="range" v-model.number="balls" min="0" max="30" step="1" name="" value="1" />
+      <p>{{ balls }}</p>
+    </div>
   </div>
 </template>
 
@@ -44,7 +48,11 @@ html {
   box-sizing: inherit;
 }
 body {
-  background: #3a3c3d url('assets/pink_dust.png') repeat top left;
+  background: #3a3c3d url('assets/hip-square_i.png') repeat top left;
+  /* ========================================================
+  This pattern is downloaded from www.subtlepatterns.com
+  If you need more, that's where to get'em.
+  ========================================================*/
   background-attachment: fixed;
   height: 100%;
   margin: 0;
@@ -64,5 +72,17 @@ body {
   align-items: center;
   align-content: center;
   padding: 2em;
+  .controls {
+    background-color: #333;
+    padding: 1em;
+    border-radius: 0.333em;
+    p {
+      color: #999;
+      margin: 0.333em 0 0 0;
+    }
+  }
+}
+input[type="range"] {
+  z-index: 10;
 }
 </style>
